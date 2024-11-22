49fa4f3991b2296996caf73f6ca0173e58b081f4
jinhoDevOps <okho04@gmail.com>
2024-11-22 23:20:11 +0900

    feat 도커 깎았음

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // 빌드 출력 경로를 'build'로 설정
+  },
+});
